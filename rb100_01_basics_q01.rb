a40d83f49b06ec42cd38a361b9f194d6e1a06d4e
Quan T <43800382+QuanT8694@users.noreply.github.com>
2024-10-29 15:51:12 +0000

    Homework questions 1-3

---
+++ rb100_01_basics_q01.rb
@@ -0,0 +1,10 @@
+=begin Add two strings together that, when concatenated, 
+return your first and last name as your full name in one string.
+=end
+
+first_name = "Quan"
+last_name = "Tran"
+
+puts "#{first_name} #{last_name}"
+
+puts "Quan " + "Tran"
---
+++ rb100_01_basics_q03.rb
@@ -0,0 +1,30 @@
+=begin
+Write a program that uses a hash to store a list of movie titles 
+with the year they came out. Then use the puts command to make your 
+program print out the year of each movie to the screen. 
+The output for your program should look something like this.
+
+1975
+2004
+2013
+2001
+1981
+
+=end
+
+movies = {
+    "pulp_fiction" => 1994,
+    "the_amazing_spider-man_2" => 2014,
+    "star_wars" => 1977,
+    "back_to_the_future" => 1985,
+    "the_silence_of_the_lambs" => 1991,
+    "jurassic_park" => 1993
+}
+
+puts movies["pulp_fiction"]
+puts movies["the_amazing_spider-man_2"]
+puts movies["star_wars"]
+puts movies["back_to_the_future"]
+puts movies["the_silence_of_the_lambs"]
+puts movies["jurassic_park"]
+
