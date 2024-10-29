4f916410b36f99c42226cf5f3d57662dbf4f7e60
Quan T <43800382+QuanT8694@users.noreply.github.com>
2024-10-29 16:36:09 +0000

    Adding next batch of questions for Basic exercises 5-7.

---
+++ rb100_01_basics_q06.rb
@@ -0,0 +1,8 @@
+=begin
+Write a program that calculates the squares of 3 float numbers of your choosing 
+and outputs the result to the screen.
+=end
+
+puts 10.0**2
+puts 0.34**2
+puts 102456**2
---
+++ rb100_01_basics_q07.rb
@@ -0,0 +1,10 @@
+=begin
+What does the following error message tell you?
+
+SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+  from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'
+
+
+Answer:
+It is saying that there is a ')' where there should be a '}'
+Perhaps it is a hash or end of a method that needs to go there?
