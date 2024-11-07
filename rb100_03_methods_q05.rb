5ef118cfdd8710de3a20bdab26589af1c18c3b8b
Quan T <43800382+QuanT8694@users.noreply.github.com>
2024-11-07 13:50:55 +0000

    Adding section 3, methods questions, to repo.

---
+++ rb100_03_methods_q05.rb
@@ -0,0 +1,24 @@
+# What will the following code print to the screen?
+
+# def scream(words)
+#     words = words + "!!!!"
+#     return
+#     puts words
+#   end
+  
+#   scream("Yippeee")
+
+=begin 
+
+1) Edit the method definition in exercise #4 
+so that it does print words on the screen. 2) What does it return now?
+
+=end
+
+def scream(words)
+    puts words = words + "!!!!"
+    return
+    puts words
+  end
+  
+  scream("Yippeee")
---
+++ rb100_03_methods_q06.rb
@@ -0,0 +1,12 @@
+=begin
+
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>'
+
+
+The method is expecting 2 arguments which was set in its parameter. One is only being passed in, so it is needing an additional argument.
+
+  =end
+
